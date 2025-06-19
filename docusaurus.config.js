2fea98c1dc07a64483a41ef07930656b525fdab3
nader dabit <dabit3@gmail.com>
2025-06-19 03:29:49 +0200

    fixed whitepaper link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import path from "node:path";
 
 const redirects = [
   {
-      from: '/eigenlayer/overview',
+      from: '/eigenlayer/overview/whitepaper',
       to: '/products/eigenlayer/concepts/eigenlayer-overview',
   },
   {
